4638c7e6b01c27db0a84f1427644c0a30f1e6937
Imam Diki Ridmawan <imamdikiridmawan@gmail.com>
2022-05-24 16:27:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <landingPage />
+</template>
+
+<script>
+import landingPage from './components/landingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    landingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/landingPage.vue
@@ -0,0 +1,509 @@
+<template>
+  <div class="lp" id="lp">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/png/komerce-logo.png" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item" id="home">
+              <a class="nav-link" aria-current="page" href="/Home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled"></a>
+            </li>
+            <li class="nav-item" id="about">
+              <a class="nav-link" aria-current="page" href="/About">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled"></a>
+            </li>
+            <li class="nav-item" id="contact">
+              <a class="nav-link" aria-current="page" href="/Contact"
+                >Contact</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled"></a>
+            </li>
+            <li class="nav-item" id="help">
+              <a class="nav-link" aria-current="page" href="/Help">Help</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <button class="btn btn-login" type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div id="section_1" class="section_1 bagian-pertama">
+      <div class="container">
+        <div class="row row-pertama justify-content-center">
+          <div class="col-xl-8 col-lg-9">
+            <div class="content">
+              <h1 class="head">
+                Landing page <br />
+                built for software.
+              </h1>
+              <div class="row bawah-head">
+                <div
+                  class="col-xl-8 col-md-8 col-sm-10 subjek"
+                  style="text-align: left"
+                >
+                  <span class="ket-head">
+                    Create custom landing pages with Shades that convert more
+                    visitors than any website—no coding required.
+                  </span>
+                </div>
+                <div class="col-xl-4 col-md-4 col-sm-6 flex-y-center">
+                  <a class="btn btn-head" href="#">Download free trial</a>
+                </div>
+                <div class="col-xl-3 col-lg-4 text-left">
+                  <div class="compitable-text border-top d-inline-block">
+                    <span class="with">Compitable with :</span>
+                    <div class="compatible-icon flex-y-center img-grayscale">
+                      <a href="#" class="font-size-13 mr-7"
+                        ><img
+                          class="gambar-icon"
+                          src="@/assets/svg/windows.svg"
+                          alt=""
+                      /></a>
+                      <a href="#" class="font-size-13 mr-7"
+                        ><img
+                          class="gambar-icon"
+                          src="@/assets/svg/apple.svg"
+                          alt=""
+                      /></a>
+                      <a href="#" class="font-size-13"
+                        ><img
+                          class="gambar-icon"
+                          src="@/assets/svg/penguine.svg"
+                          alt=""
+                      /></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section_2" class="section_2 bagian-kedua">
+      <div class="container">
+        <div class="row row-kedua justify-content-center">
+          <div
+            class="col-lg-9"
+          >
+            <img
+              src="../assets/png/laptop-screen.png"
+              alt=""
+              class="laptop-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section_3" class="section_3 bagian-ketiga">
+      <div class="container">
+        <div class="row row-ketiga justify-content-center">
+          <div
+            class="col-lg-12"
+          >
+            <div
+              class="
+                brand-logos
+                d-flex
+                justify-content-center justify-content-xl-between
+                align-items-center
+                flex-wrap
+              "
+            >
+              <div class="single-brand">
+                <img src="../assets/svg/brand-logo.svg" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="../assets/svg/brand-logo-1.svg" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="../assets/svg/brand-logo-2.svg" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="../assets/svg/brand-logo-3.svg" alt="" />
+              </div>
+              <div class="single-brand">
+                <img src="../assets/svg/brand-logo-4.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section_4" class="section_4 bagian-keempat">
+      <div class="container">
+        <div
+          class="
+            row row-keempat
+            align-items-center
+            justify-content-lg-start justify-content-center
+          "
+        >
+          <div
+            class="col-xl-6 col-md-5 col-md-10"
+          >
+            <div class="content-img position-relative z-index-1">
+              <img src="../assets/png/content-img1.png" alt="" class="w-100" />
+            </div>
+          </div>
+          <div
+            class="
+              offset-xxl-1
+              col-xxl-5 col-xl-6 col-lg-7 col-md-9"
+          >
+            <div class="content section-heading-5">
+              <h2>Build Beautiful Landing Pages Faster.</h2>
+              <p>
+                Create custom landing pages with Shades that convert more
+                visitors than any website, no coding required.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section_5" class="section_5 bagian-kelima">
+      <div class="container">
+        <div
+          class="
+            row
+            align-items-center
+            justify-content-lg-between justify-content-center
+          "
+        >
+          <div
+            class="
+              col-xxl-4 col-xl-5 col-lg-6 col-md-9"
+          >
+            <div class="content section-heading-5">
+              <h2>Completely Free for Everyone.</h2>
+              <div class="d-flex content-5-card">
+                <img src="../assets/png/people.png" width="32px" alt="">
+                <div class="content-body">
+                  <h5>Easy setup process</h5>
+                  <p>
+                    Editing and customizing Essential Landing is easy and fast.
+                  </p>
+                </div>
+              </div>
+              <div class="d-flex content-5-card">
+                <img src="../assets/png/settings.png" width="32px" alt="icon" />
+                <div class="content-body">
+                  <h5>Built for real users</h5>
+                  <p>
+                    Editing and customizing Essential Landing is easy and fast.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="
+              col-xxl-6
+              offset-xxl-2
+              col-xl-6 col-lg-5 col-md-9"
+          >
+            <div class="content-img">
+              <img
+                src="../assets/png/content-img2.png"
+                alt=""
+                class="w-lg-auto w-100"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section_6" class="section_6 bagian-keenam">
+        <div class="container">
+            <div class="row align-items-center justify-content-center justify-content-lg-start">
+                <div class="col-xl-6 col-lg-6 col-md-8">
+                    <div class="content-img">
+                    <img src="../assets/png/content-img3.png" alt="" class="w-100">
+                    </div>
+                </div>
+                <div class="offset-xl-1 col-xl-5 col-lg-6 col-md-9">
+                    <div class="content section-heading-5">
+                        <h2>Completely Free for Everyone.</h2>
+                        <p>Event is not like most tech conferences. We want our presentations to engage the audience, spark
+                            discussion and inspire new ideas. </p>
+                        <ul class="list-unstyled pl-0">
+                            <li class="d-flex align-items-center">
+                            <div class="col-1">
+                                <img src="../assets/png/check.png" alt="">
+                            </div>
+                            <span>Unlimited design possibility</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                            <div class="col-1">
+                                <img src="../assets/png/check.png" alt="">
+                            </div>
+                            <span>Completely responsive</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                            <div class="col-1">
+                                <img src="../assets/png/check.png" alt="">
+                            </div>
+                            <span>Easy to customize</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  padding: 0%;
+  margin: 0%;
+}
+img {
+  max-width: 100%;
+}
+/* Navbar */
+li {
+  margin-bottom: -5px;
+}
+nav {
+  border-bottom: 1px solid #fafa;
+}
+.navbar-light .navbar-nav a.nav-link {
+  color: #000;
+  text-decoration: none;
+
+  font-family: Karla;
+  font-size: 16px;
+  font-weight: 500;
+  font-style: normal;
+  letter-spacing: 0.5px;
+}
+
+.navbar-light .navbar-nav a.nav-link:hover {
+  color: rgb(255, 0, 0);
+}
+
+.nav-link::after {
+  content: "";
+  display: block;
+  width: 0;
+  height: 2px;
+  background: rgb(255, 0, 0);
+  transition: width 0.3s;
+}
+
+.nav-link:hover::after {
+  width: 100%;
+  transition: width 0.3s;
+}
+button.btn-login {
+  color: #ffffff;
+  background: rgb(255, 0, 0);
+}
+button.btn-login:hover {
+  color: rgb(255, 0, 0);
+  background: transparent;
+  border: solid 1px rgb(255, 0, 0);
+}
+/* section_1 */
+#section_1 {
+  padding-top: 150px;
+  padding-bottom: 70px;
+
+  /* background-image: url('../assets/png/bg-8.jpg'); 
+        background-repeat: no-repeat;
+        background-size: cover; */
+}
+.head {
+  font-family: "Gothic A1", sans-serif;
+  color: #000;
+  font-size: 50px;
+  font-weight: 800;
+  font-style: normal;
+  letter-spacing: -3px;
+  /* line-height: 55px; */
+  margin-bottom: 38px;
+  text-align: left;
+}
+.ket-head {
+  font-family: Karla;
+  color: #000;
+  font-size: 21px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.4px;
+  line-height: 36px;
+}
+.compitable-text,
+.compatible-icon {
+  margin-top: 15px;
+}
+.border-top {
+  border-top: 1px solid #dee2e6 !important;
+  padding-top: 20px;
+}
+.text-left {
+  text-align: left;
+}
+.flex-y-center {
+  display: flex;
+  align-items: center;
+}
+.compitable-text .with {
+  font-family: Karla;
+  color: #000;
+  font-size: 21px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.4px;
+  line-height: 36px;
+}
+.gambar-icon {
+  padding-left: 6px;
+  padding-right: 6px;
+}
+.btn.btn-head {
+  min-width: 205px;
+  height: 60px;
+  background: rgb(255, 0, 0);
+  border-radius: 3px;
+  font-family: Karla;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+  letter-spacing: -0.9px;
+  color: #ffffff;
+  padding-top: 17px;
+}
+.btn.btn-head:hover {
+  color: rgb(255, 0, 0);
+  background-color: transparent;
+  border: rgb(255, 0, 0) solid 1px;
+}
+/* .btn.btn-outline-light:hover {
+        background-color: transparent;
+    } */
+/* section_2 */
+/* section_3 */
+.section_3 .brand-logos .single-brand {
+  margin: 0px 20px 45px 20px;
+}
+/* section_4 */
+#section_4 {
+  padding-top: 130px;
+}
+.section-heading-5 h2 {
+  font-family: Gothic A1;
+  color: #1f1f1f;
+  font-size: 48px;
+  font-weight: 800;
+  font-style: normal;
+  letter-spacing: -2.52632px;
+  line-height: 64px;
+  margin-bottom: 35px;
+  text-align: left;
+}
+.section-heading-5 p {
+  font-family: Karla;
+  color: #333333;
+  font-size: 21px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.4px;
+  line-height: 36px;
+  margin-bottom: 0;
+  text-align: left;
+}
+/* section_5 */
+#section_5 {
+    padding-top: 125px;
+    padding-bottom: 110px;
+}
+.content .content-5-card {
+    border: 1px solid #e5e5e5 !important;
+    border-radius: 5px;
+    padding: 20px 35px 20px 35px;
+    margin-bottom: 15px;
+    align-items: end;
+}
+.content-5-card img {
+    margin-right: 25px;
+}
+.content p {
+    font-family: Karla;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 30px;
+    letter-spacing: -0.4px;
+    color: #333333;
+}
+.content-body h5 {
+    text-align: left;
+    font-family: "Gothic A1";
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 30px;
+    color: #000000;
+}
+/* section-6 */
+#section_6 {
+    padding-bottom: 120px;
+}
+.section-heading-5 p {
+    font-family: Karla;
+    color: #333333;
+    font-size: 21px;
+    font-weight: 400;
+    font-style: normal;
+    letter-spacing: -0.4px;
+    line-height: 36px;
+    margin-bottom: 0;
+}
+.section-heading-5 ul {
+    margin-top: 50px;
+    margin-bottom: -20px;
+}
+.section-heading-5 ul li {
+    font-family: Karla;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 32px;
+    letter-spacing: -1.4px;
+    color: #000000;
+    margin-bottom: 20px;
+}
+.section-heading-5 ul li span {
+    padding-left: 20px;
+}
+</style>
