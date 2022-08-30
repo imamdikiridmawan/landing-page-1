91151ad2a718fdb94aa7c83a152b7e0c78698031
Imam Diki Ridmawan <imamdikiridmawan@gmail.com>
2022-08-30 09:04:16 +0700

    add tailwind

---
+++ src/components/footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="Footer">
+    <footer class="footer text-center text-md-start">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="footer-logo">
+              <a href="#"><img src="../assets/png/logo-baru-komerce.png" alt="logo"></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="footer-widget">
+              <ul class="pl-0 list-unstyled d-flex flex-wrap justify-content-center align-items-end ">
+                <li class="d-flex"><a href="/about">About</a></li>
+                <li class="d-flex"><a href="/feature">Features</a></li>
+                <li class="d-flex"><a href="/works">Works</a></li>
+                <li class="d-flex"><a href="/career">Career</a></li>
+                <li class="d-flex"><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="social-icons">
+              <ul class="pl-0 list-unstyled d-flex  justify-content-center justify-content-md-end align-items-end ">
+                <li class="d-flex flex-column justify-content-center"><a href="#"><img src="../assets/png/twitter.png" alt=""></a></li>
+                <li class="d-flex flex-column justify-content-center"><a href="#"><img src="../assets/png/facebook.png" alt=""></a></li>
+                <li class="d-flex flex-column justify-content-center"><a href="#"><img src="../assets/png/instagram.png" alt=""></a></li>
+                <li class="d-flex flex-column justify-content-center"><a href="#"><img src="../assets/png/github.png" alt=""></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="text-center">
+              <p>© Grayic 2021 All right reserved. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="../assets/css/footer.css" />
---
+++ src/components/header.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="section_nav" class="section_nav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+            <img src="@/assets/png/komerce-logo.png" alt="" />
+            </a>
+            <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link disabled"></a>
+                </li>
+                <li class="nav-item" id="about">
+                <a class="nav-link" aria-current="page" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled"></a>
+                </li>
+                <li class="nav-item" id="feature">
+                <a class="nav-link" aria-current="page" href="/feature">Feature</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled"></a>
+                </li>
+                <li class="nav-item" id="works">
+                <a class="nav-link" aria-current="page" href="/works">Works</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled"></a>
+                </li>
+                <li class="nav-item" id="career">
+                <a class="nav-link" aria-current="page" href="/career">Career</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled"></a>
+                </li>
+                <li class="nav-item" id="contact">
+                <a class="nav-link" aria-current="page" href="/contact">Contact</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-login" type="submit">Login</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="../assets/css/header.css" />
